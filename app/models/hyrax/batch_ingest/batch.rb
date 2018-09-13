67137d3af24d5b5da5d83497e47821c5ff6dd2ac
Chris Colvard <cjcolvar@indiana.edu>
2018-09-13 11:52:33 -0400

    Fix typo

--- app/models/hyrax/batch_ingest/batch.rb
+++ app/models/hyrax/batch_ingest/batch.rb
@@ -5,7 +5,7 @@ module Hyrax
       has_many :batch_items
 
       class_attribute :reader # Reads the batch and creates batch items
-      class_attribute :item_validator # Dry::Validatpr.Schema
+      class_attribute :item_validator # Dry::Validator.Schema
       class_attribute :item_processor # Dry::Transaction
 
       def initialize(filename: filename, submitter: submitter)
