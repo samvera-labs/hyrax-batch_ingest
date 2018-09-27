b71f15f377230b01d298241b45741b47e6c69521
Andrew Myers <andrew_myers@wgbh.org>
2018-09-27 09:57:28 -0400

    Install Hyrax as part of test app setup
    closes #21

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -6,6 +6,9 @@ class TestAppGenerator < Rails::Generators::Base
   # if you need to generate any additional configuration
   # into the test app, this generator will be run immediately
   # after setting up the application
+  def install_hyrax
+    generate 'hyrax:install -f'
+  end
 
   def install_engine
     generate 'hyrax-batch_ingest:install'
