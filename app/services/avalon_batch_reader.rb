2e0a6b4766afaee8edec96e117c13038fd700a8d
yingfeng-iu <yingfeng@iu.edu>
2018-11-01 17:07:58 -0400

    Issue#49: add BatchReader class as a common interface

--- app/services/avalon_batch_reader.rb
+++ app/services/avalon_batch_reader.rb
@@ -1,34 +1,10 @@
-class AvalonBatchReader
+class AvalonBatchReader < BatchReader
 
   FILE_FIELDS = [:file,:label,:offset,:skip_transcoding,:absolute_location,:date_digitized]
   SKIP_FIELDS = [:collection]
 
-  def initialize(source_location)
-    @source_location = source_location
-    @read = false
-    @name = nil
-    @submitter_email = nil
-    @batch_items = nil
-  end
+  protected
 
-  # TODO refer to Issue #58 to decide if we will add/populate this field
-  def name
-    read unless @read
-    @name
-  end
-
-  def submitter_email
-    read unless @read
-    @submitter_email
-  end
-
-  def batch_items
-    read unless @read
-    @batch_items
-  end
-
-  private
-  
   def read()
     begin
       spreadsheet = Roo::Spreadsheet.open(source_location)
@@ -40,6 +16,8 @@ class AvalonBatchReader
       @read = true
     end
   end
+
+  private
 
   def read_name_email(spreadsheet)
     @name = spreadsheet.row(spreadsheet.first_row)[0]
