9801a4c66e7ad388a370d7e29f6d24f6af4a5e14
yingfeng-iu <yingfeng@iu.edu>
2018-11-01 16:22:27 -0400

    Issue#49: add AvalonBatchReader class

---
+++ app/services/avalon_batch_reader.rb
@@ -0,0 +1,121 @@
+class AvalonBatchReader
+
+  EXTENSIONS = ['csv','xls','xlsx','ods']
+  FILE_FIELDS = [:file,:label,:offset,:skip_transcoding,:absolute_location,:date_digitized]
+  SKIP_FIELDS = [:collection]
+
+  def initialize(source_location)
+    @source_location = source_location
+    @read = false
+    @name = nil
+    @submitter_email = nil
+    @batch_items = nil
+  end
+
+  # TODO refer to Issue #58 to decide if we will add/populate this field
+  def name
+    read unless @read
+    @name
+  end
+
+  def submitter_email
+    read unless @read
+    @submitter_email
+  end
+
+  def batch_items
+    read unless @read
+    @batch_items
+  end
+
+  private
+  
+  def read()
+    begin
+      # batch.name = spreadsheet.row(spreadsheet.first_row)[0] # TODO add name to batch DB
+      # batch.submitter_email = spreadsheet.row(spreadsheet.first_row)[1]
+      # # batch.admin_set_id = admin_set_id
+      # batch.source_location = ''  # TODO what value?
+
+      spreadsheet = Roo::Spreadsheet.open(source_location)
+      read_batch_items(spreadsheet, field_names(spreadsheet))
+      
+      # batch.status = :accepted
+    rescue Exception => err
+      # batch.status = :error
+      # batch.error = err
+      raise ReaderError "Invalid manifest file: #{err.message}"
+    ensure
+      @read = true
+    end
+  end
+
+  def read_name_email
+    @name = @spreadsheet.row(@spreadsheet.first_row)[0]
+    @submitter_email = @spreadsheet.row(@spreadsheet.first_row)[1]
+  end
+
+  def field_names(spreadsheet)
+    header_row = spreadsheet.row(spreadsheet.first_row + 1)
+    field_names = header_row.collect { |field|
+      field.to_s.downcase.gsub(/\s/,'_').strip.to_sym
+    }
+  end
+  
+  def read_batch_items(spreadsheet, field_names)
+    @batch_items = []
+    first = spreadsheet.first_row + 2
+    last = spreadsheet.last_row
+    first.upto(last) do |index|
+      opts = {
+          :publish => false,
+          :hidden  => false
+      }
+
+      values = spreadsheet.row(index).collect do |val|
+        (val.is_a?(Float) and (val == val.to_i)) ? val.to_i.to_s : val.to_s
+      end
+
+      content=[]
+      fields = Hash.new { |h,k| h[k] = [] }
+      field_names.each_with_index do |f,i|
+        unless f.blank? || SKIP_FIELDS.include?(f) || values[i].blank?
+          if FILE_FIELDS.include?(f)
+            content << {} if f == :file
+            content.last[f] = f == :skip_transcoding ? true?(values[i]) : values[i]
+          else
+            fields[f] << values[i]
+          end
+        end
+      end
+
+      opts.keys.each { |opt|
+        val = Array(fields.delete(opt)).first.to_s
+        if opts[opt].is_a?(TrueClass) or opts[opt].is_a?(FalseClass)
+          opts[opt] = true?(val)
+        else
+          opts[opt] = val
+        end
+      }
+
+      files = content.each { |file| file[:file] = path_to(file[:file]) }
+      @batch_items << create_batch_item(index, fields, files)
+    end
+  end
+
+  def create_batch_item(index, fields, files)
+      batch_item = BatchItem.new
+      # batch_item.batch = batch
+      batch_item.id_within_batch = index.to_s
+      batch_item.source_location = @source_location
+      batch_item.status = :accepted
+      batch_item.error = ''
+      batch_item.object_id = ''  # TODO
+
+      all_fields = fields.select { |f| !FILE_FIELDS.include?(f) }
+      all_fields << { :files => files }
+      batch_item.source_data = all_fields.to_json
+
+      batch_item
+  end
+end
