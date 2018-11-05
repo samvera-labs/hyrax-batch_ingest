c9d9c5bb8cb42a1c745552306f3e3cd787049fd0
Chris Colvard <cjcolvar@indiana.edu>
2018-11-05 13:33:01 -0500

    Fix foreign key and index; fold ingest_type into create migration

--- db/migrate/20181022203038_create_hyrax_batch_ingest_batches.rb
+++ db/migrate/20181022203038_create_hyrax_batch_ingest_batches.rb
@@ -6,6 +6,7 @@ class CreateHyraxBatchIngestBatches < ActiveRecord::Migration[5.1]
       t.string :source_location
       t.text :error
       t.string :admin_set_id
+      t.string :ingest_type
 
       t.timestamps
     end
--- db/migrate/20181031192446_add_ingest_type_to_hyrax_batch_ingest_batches.rb
+++
@@ -1,5 +0,0 @@
-class AddIngestTypeToHyraxBatchIngestBatches < ActiveRecord::Migration[5.1]
-  def change
-    add_column :hyrax_batch_ingest_batches, :ingest_type, :string
-  end
-end
