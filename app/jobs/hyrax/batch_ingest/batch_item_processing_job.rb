8b5535e8ade2e77577bb452a731ee96aaf4e9cd2
Chris Colvard <cjcolvar@indiana.edu>
2018-11-09 13:39:34 -0500

    Refactor BatchItemProcessingJob

--- app/jobs/hyrax/batch_ingest/batch_item_processing_job.rb
+++ app/jobs/hyrax/batch_ingest/batch_item_processing_job.rb
@@ -2,26 +2,36 @@
 module Hyrax
   module BatchIngest
     class BatchItemProcessingJob < ApplicationJob
-      def perform(batch_item)
+      before_perform do
+        batch_item = arguments.first
         batch_item.batch.update(status: 'running') if batch_item.batch.status == 'enqueued'
         batch_item.update(status: 'running')
-        config = Hyrax::BatchIngest::Config.new(batch_item.batch.ingest_type)
-        validation_result = config.source_validator.validate(batch_item)
-        if validation_result.failure?
-          notify_invalid(batch_item, validation_result)
-          return
-        end
-        # TODO: read batch_item.source_data or batch_item.source_location and feed into mapper?
-        work = config.mapper.map(batch_item)
+      end
+
+      after_perform do
+        batch_item = arguments.first
+        batch_item.batch.update(status: 'completed') if batch_item.batch.completed?
+      end
+
+      rescue_from(StandardError) do |exception|
+        batch_item = arguments.first
+        notify_failed(batch_item, exception)
+        batch_item.batch.update(status: 'completed') if batch_item.batch.completed?
+      end
+
+      def perform(batch_item)
+        config = Hyrax::BatchIngest.config.ingest_types[batch_item.batch.ingest_type.to_sym]
+        # validation_result = config.source_validator.new(batch_item).validate
+        # if validation_result.failure?
+        #   notify_invalid(batch_item, validation_result)
+        #   return
+        # end
+        work = config.mapper.new(batch_item).map
         if work.save
           batch_item.update(status: 'completed', object_id: work.id)
         else
           notify_failed_save(batch_item, work)
         end
-      rescue StandardError => e
-        notify_failed(batch_item, e)
-      ensure
-        batch_item.batch.update('completed') if batch_item.batch.completed?
       end
 
       private
@@ -30,9 +40,9 @@ module Hyrax
           batch_item.update(status: 'failed', error: work.errors.full_messages.join(" "))
         end
 
-        def notify_invalid(batch_item, validation_result)
-          batch_item.update(status: 'failed', error: validation_result.messages(full: true).values.flatten.join(" "))
-        end
+        # def notify_invalid(batch_item, validation_result)
+        #   batch_item.update(status: 'failed', error: validation_result.messages(full: true).values.flatten.join(" "))
+        # end
 
         def notify_failed(batch_item, exception)
           batch_item.update(status: 'failed', error: exception.message)
