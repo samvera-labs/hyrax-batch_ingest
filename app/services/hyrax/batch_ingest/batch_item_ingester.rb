0850c4c5925b0773c26b2d68e51d3fb9f3a0ec93
Chris Colvard <cjcolvar@indiana.edu>
2018-11-14 14:11:44 -0500

    Add tests for abstract reader and ingester classes.

--- app/services/hyrax/batch_ingest/batch_item_ingester.rb
+++ app/services/hyrax/batch_ingest/batch_item_ingester.rb
@@ -2,12 +2,14 @@
 module Hyrax
   module BatchIngest
     class BatchItemIngester
+      attr_reader :batch_item
+
       def initialize(batch_item)
         @batch_item = batch_item
       end
 
       def ingest
-        raise Hyrax::BatchIngest::IngesterError.new("Cannot use abstract Ingester class.")
+        raise Hyrax::BatchIngest::IngesterError.new("Cannot use abstract BatchItemIngester class.")
       end
     end
   end
--- app/services/hyrax/batch_ingest/batch_reader.rb
+++ app/services/hyrax/batch_ingest/batch_reader.rb
@@ -3,19 +3,21 @@
 module Hyrax
   module BatchIngest
     class BatchReader
+      attr_reader :source_location
+
       def initialize(source_location)
         @source_location = source_location
         @read = false
-        @name = nil
+        # @name = nil
         @submitter_email = nil
         @batch_items = nil
       end
 
-      # TODO: refer to Issue #58 to decide if we will add/populate this field
-      def name
-        read unless @read
-        @name
-      end
+      # # TODO: refer to Issue #58 to decide if we will add/populate this field
+      # def name
+      #   read unless @read
+      #   @name
+      # end
 
       def submitter_email
         read unless @read
@@ -29,7 +31,9 @@ module Hyrax
 
       protected
 
-        def read; end
+        def read
+          raise Hyrax::BatchIngest::ReaderError.new("Cannot use abstract BatchReader class.")
+        end
     end
   end
 end
