18ec864b83ab750b2fcf3f8c63a38fe633c20924
cjcolvar <cjcolvar@indiana.edu>
2018-11-16 13:47:24 -0500

    Remove route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 Hyrax::BatchIngest::Engine.routes.draw do
-  resources :batches, only: [:index, :show, :new, :create] do
-    resources :items, only: [:show], controller: 'batch_items'
-  end
+  resources :batches, only: [:index, :show, :new, :create]
 end
