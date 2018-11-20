5fcfd7f26f4de94adc76f94d37548f3b98f3f415
cjcolvar <cjcolvar@indiana.edu>
2018-11-20 09:37:17 -0500

    Delete batch_items_controller.rb (#97)
    * Delete `batch_items_controller.rb`
    * Remove route test for `BatchItemsController`
    * Remove route config for `batch_items`

--- app/controllers/hyrax/batch_ingest/batch_items_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module Hyrax
-  module BatchIngest
-    class BatchItemsController < Hyrax::BatchIngest::ApplicationController
-      def show; end
-    end
-  end
-end
