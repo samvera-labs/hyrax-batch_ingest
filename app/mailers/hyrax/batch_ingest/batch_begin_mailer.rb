72cdf081a56199d994a237a09e49e0b9a1dd9dde
Brian Keese <bkeese@indiana.edu>
2018-11-27 10:00:38 -0500

    Fix receiver

--- app/mailers/hyrax/batch_ingest/batch_begin_mailer.rb
+++ app/mailers/hyrax/batch_ingest/batch_begin_mailer.rb
@@ -3,19 +3,19 @@ module Hyrax::BatchIngest
   class BatchBeginMailer < ApplicationMailer
     def batch_started_successfully
       @batch = params[:batch]
-      mail(to: @batch.submitter_email, subject: 'Hyrax Batch Ingest Started (Success)')
+      mail(to: receiver, subject: 'Hyrax Batch Ingest Started (Success)')
     end
 
     def batch_started_with_errors
       @batch = params[:batch]
-      mail(to: @batch.submitter_email, subject: 'Hyrax Batch Ingest Started (With Errors)')
+      mail(to: receiver, subject: 'Hyrax Batch Ingest Started (With Errors)')
     end
 
     private
 
       def receiver
         # TODO: get default receiver from config
-        params[:submitter_email].present? ? params[:submitter_email] : 'admin@example.com'
+        @batch.submitter_email.present? ? @batch.submitter_email : 'admin@example.com'
       end
   end
 end
