e9cb2b975040208355ab689f733e649fdb2d58c2
yingfeng-iu <yingfeng@iu.edu>
2018-12-26 11:03:07 -0500

    Issue#74: move batch side bar link from Activties to Repository Contents

--- spec/views/hyrax/dashboard/batch_sidebar_spec.rb
+++ spec/views/hyrax/dashboard/batch_sidebar_spec.rb
@@ -1,33 +1,63 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
+# describe 'hyrax/dashboard/_sidebar.html.erb', type: :view do
 # describe 'hyrax/dashboard/sidebar/_activity.html.erb', type: :view do
-describe 'hyrax/dashboard/_sidebar.html.erb', type: :view do
+describe 'hyrax/dashboard/sidebar/_repository_content.html.erb', type: :view do
   # let(:user) { stub_model(User, user_key: 'mjg', name: 'Foobar') }
   let(:user) { create(:user) }
+  let(:menu) { Hyrax::MenuPresenter.new(view) }
 
   before do
     allow(view).to receive(:signed_in?).and_return(true)
     allow(view).to receive(:current_user).and_return(user)
     # assign(:user, user)
     allow(view).to receive(:can?).with(:index, Hyrax::BatchIngest::Batch).and_return(:can_view_batches)
+    allow(view).to receive(:can?).with(:read, :admin_dashboard).and_return(true)
+    # allow(view).to receive(:can?).with(:manage_any, AdminSet).and_return(true)
+    # allow(view).to receive(:can?).with(:review, :submissions).and_return(true)
+    # allow(view).to receive(:can?).with(:manage, User).and_return(true)
+    # allow(view).to receive(:can?).with(:update, :appearance).and_return(true)
+    # allow(view).to receive(:can?).with(:manage, Hyrax::Feature).and_return(true)
+    # allow(view).to receive(:can?).with(:manage, Sipity::WorkflowResponsibility).and_return(true)
+    # allow(view).to receive(:can?).with(:manage, :collection_types).and_return(true)
+    # render 'hyrax/dashboard/sidebar/repository_content', menu: menu
+    # subject { rendered }
   end
+
+  # it 'has the Collection link' do
+  #   is_expected.to have_link t('hyrax.admin.sidebar.collections')
+  # end
+  #
+  # it 'has the Work link' do
+  #   is_expected.to have_link t('hyrax.admin.sidebar.works')
+  # end
 
   context 'as a user who can view Batch info' do
     let(:can_view_batches) { true }
 
-    before { render }
+    # before { render 'hyrax/dashboard/sidebar/activity', menu: menu }
+    before { render 'hyrax/dashboard/sidebar/repository_content', menu: menu }
     subject { rendered }
 
     it 'has the Batches link' do
       is_expected.to have_link 'Batches'
     end
+
+    it 'has the Collection link' do
+      is_expected.to have_link t('hyrax.admin.sidebar.collections')
+    end
+
+    it 'has the Work link' do
+      is_expected.to have_link t('hyrax.admin.sidebar.works')
+    end
+
   end
 
   context 'as a user who cannot view Batch info' do
     let(:can_view_batches) { false }
 
-    before { render }
+    before { render 'hyrax/dashboard/sidebar/repository_content', menu: menu}
     subject { rendered }
 
     it 'does not have the Batches link' do
