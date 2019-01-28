3808c5e4c5cd788812bae027ba053eae6eceace8
yingfeng-iu <yingfeng@iu.edu>
2019-01-28 16:33:25 -0500

    issue#10: fixing tests, can't test on run

--- lib/hyrax/batch_ingest/spec/shared_specs/batch_scanner.rb
+++ lib/hyrax/batch_ingest/spec/shared_specs/batch_scanner.rb
@@ -21,30 +21,17 @@ RSpec.shared_examples "a Hyrax::BatchIngest::BatchScanner" do
     before do
       allow(admin_set).to receive(:id).and_return(0)
       allow(Hyrax::BatchIngest::BatchRunner).to receive(:new).and_call_original
-      run_count = 0
-      allow_any_instance_of(Hyrax::BatchIngest::BatchRunner).to receive(:run) { run_count += 1 }
-      # manifests.each_with_index do |manifest, i|
-      #   allow(Hyrax::BatchIngest::BatchRunner).to receive(:new).with(ingest_type: 'Avalon Ingest Type', source_location: manifest, admin_set_id: admin_set.id).and_return(runners[i])
-      #   allow(runners[i]).to receive(:run)
-      # end
+      @run_count = 0
+      allow_any_instance_of(Hyrax::BatchIngest::BatchRunner).to receive(:run) { @run_count += 1 }
     end
-
-    # subject { scanner.scan }
-    # let(:runners) { manifests.collect { |manifest| double('Hyrax::BatchIngest::BatchRunner') } }
-    # let(:runners) { manifests.collect { Hyrax::BatchIngest::BatchRunner.new } }
-    # let(:runners) { manifest.collect { |manifest| Hyrax::BatchIngest::BatchRunner.new(ingest_type: 'Avalon Ingest Type', source_location: manifest, admin_set_id: admin_set.id) } }
 
     it 'creates/run BatchRunner for each unprocessed manifest' do
       scanner.scan
       expect(Hyrax::BatchIngest::BatchRunner).to have_received(:new).exactly(manifests.count).times
-      expect_any_instance_of(Hyrax::BatchIngest::BatchRunner).to have_received(:run).exactly(manifests.count).times
-      # expect(run_count).to eq(manifests.count)
+      expect(@run_count).to eq(manifests.count)
       manifests.each do |manifest|
         expect(Hyrax::BatchIngest::BatchRunner).to have_received(:new).with(ingest_type: 'Avalon Ingest Type', source_location: manifest, admin_set_id: admin_set.id)
       end
-      # manifests.each_with_index do |manifest, i|
-      #   expect(runners[i].to have_received(:run))
-      # end
     end
   end
 end
