077736088f4b2bb0e33d9523e99dd27a13f9d321
Andrew Myers <andrew_myers@wgbh.org>
2019-05-04 17:49:05 -0400

    Links to the object ingested by the batch item
    Also,
    * Ensure that PRs that include a change in the version number will result in a
      fresh internal test app.
    * Fixes test app generator's source_root, fixes the generator's methods that
      call copy_file, which allows the generator to run to completion, specifically
      generating GenericWork, which is now used in some specs.

--- app/presenters/hyrax/batch_ingest/batch_item_presenter.rb
+++ app/presenters/hyrax/batch_ingest/batch_item_presenter.rb
@@ -6,7 +6,7 @@ module Hyrax
       attr_reader :batch_item
 
       delegate :id, :id_within_batch, :source_location, :error, :repo_object_id,
-               :status, to: :batch_item
+               :status, :repo_object_class_name, to: :batch_item
 
       def initialize(batch_item)
         @batch_item = batch_item
@@ -17,11 +17,24 @@ module Hyrax
       end
 
       def status_label
-        self.class.status_labels[status] || 'unknown'
+        self.class.status_labels[status]
       end
 
       def status_css_class
         "clickable #{self.class.status_css_classes[batch_item.status]}"
+      end
+
+      # @return [String] the relative URL to the #show page for the repository
+      #  object corresponding to the :repo_object_id value of the batch item.
+      def repo_object_path
+        return nil unless repo_object_path_helper && repo_object_id
+        send(repo_object_path_helper, repo_object_id)
+      end
+
+      # @return [String] the human-readable name of the model if we can get it;
+      #  otherwise returns the :repo_object_class_name for the batch item as-is.
+      def repo_object_display_name
+        repo_object_class&.model_name&.human || repo_object_class_name
       end
 
       class << self
@@ -45,6 +58,20 @@ module Hyrax
           }.with_indifferent_access
         end
       end
+
+      private
+
+        def repo_object_class
+          Object.const_get(batch_item.repo_object_class_name.to_s)
+        rescue NameError
+          nil
+        end
+
+        def repo_object_path_helper
+          return nil unless repo_object_class
+          self.class.include Rails.application.routes.url_helpers
+          "hyrax_#{repo_object_class.model_name.singular}_path"
+        end
     end
   end
 end
